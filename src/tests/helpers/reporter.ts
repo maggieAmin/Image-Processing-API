5a0df2d936412d54601b1933a7747ce374dd654f
Maggie Amin 😎 <maggiemmagdy@gmail.com>
2022-03-21 20:08:55 +0000

    Initial project setup 🚀

---
+++ src/tests/helpers/reporter.ts
@@ -0,0 +1,17 @@
+import { DisplayProcessor, SpecReporter, StacktraceOption } from "jasmine-spec-reporter";
+import SuiteInfo = jasmine.SuiteInfo;
+
+class CusomProcessor extends DisplayProcessor {
+    public displayJasmineStarted(info: SuiteInfo, log: string) {
+        return `${log}`;
+    }
+}
+
+jasmine.getEnv().clearReporters();
+jasmine.getEnv().addReporter(new SpecReporter({
+    spec: {
+        displayStacktrace: StacktraceOption.NONE
+    },
+    customProcessors: [CusomProcessor],
+}));
+    
